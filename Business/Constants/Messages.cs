82e97c71d56b4c542321f182a12a773526fa5281
Tolgahan Yiğit Özdemir <tolgahanyigitozdemir@hotmail.com>
2022-09-02 21:34:05 +0300

    .

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +15,6 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string ProductsListed = "Ürünler listelendi";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string AuthorizationDenied = "Hoop Hemşerim Nereye???";
     }
 }
